cfabafcc4fe6924d5fdc1f22b3a6b35955489f6c
bullfight <p.schmitz@gmail.com>
2011-06-08 11:08:44 +0200

    encrypted password implimented

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require 'Digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -38,10 +38,19 @@ class User < ActiveRecord::Base
   private
   
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)      
     end
     
     def encrypt(string)
-      string
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
