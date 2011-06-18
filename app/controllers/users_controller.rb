cbbce7a0b8fde70fa6baf93c960cce4df39af70b
bullfight <p.schmitz@gmail.com>
2011-06-19 00:09:27 +0200

    added notices to redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
-  before_filter :normal_user,  :only => [:new, :create]
+  before_filter :sign_out_first,  :only => [:new, :create]
   
   def index
     @title = "All users"
@@ -66,11 +66,13 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_path, 
+        :notice => "Must be admin to perform this action") unless current_user.admin?
     end
     
-    def normal_user
-      redirect_to(root_path) unless signed_in? == false
+    def sign_out_first
+      redirect_to(root_path, 
+        :notice => "Sign out to create a new user") unless signed_in? == false
     end
 
 end
