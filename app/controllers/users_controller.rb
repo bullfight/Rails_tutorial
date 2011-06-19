af4c472095345f3ae5be0d39575debea487ab4cf
bullfight <p.schmitz@gmail.com>
2011-06-19 12:55:19 +0200

    Added microposts to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.page(params[:page]).per(10)
     @title = @user.name
   end
     
