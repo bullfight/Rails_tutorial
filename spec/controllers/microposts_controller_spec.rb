2143177857a25d3eea836024cb6cbcc8f3b10ba9
bullfight <p.schmitz@gmail.com>
2011-06-19 18:02:38 +0200

    destroy authorization and tests for micropost

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -62,4 +62,38 @@ describe MicropostsController do
     end # success
   
   end # Post create
+  
+  describe "DELETE 'destroy'" do
+    
+    describe "for an unauthorized user" do
+            
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        test_sign_in(wrong_user)
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should deny access" do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+      
+    end # unauthorized user
+    
+    describe "for an authorized user" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should destroy the micropost" do
+        lambda do 
+          delete :destroy, :id => @micropost
+        end.should change(Micropost, :count).by(-1)
+      end
+      
+    end # authorized user
+  end # Delete destroy
 end
