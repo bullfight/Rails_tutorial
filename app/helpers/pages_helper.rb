8c0c633d56005e22c80809533ba3e11a58da4a44
bullfight <p.schmitz@gmail.com>
2011-06-23 19:06:12 +0200

    added paginator for more

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def link_to_next_page(scope, name, options = {}, &block)
+    params = options.delete(:params) || {}
+    param_name = options.delete(:param_name) || Kaminari.config.param_name
+    link_to_unless scope.last_page?, name, params.merge(param_name => (scope.current_page + 1)), options.merge(:rel => 'next') do
+      block.call if block
+    end
+  end
 end
