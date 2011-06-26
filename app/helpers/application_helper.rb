946e8d73f04c389be473996cc39c45aefe690de4
bullfight <p.schmitz@gmail.com>
2011-06-26 22:06:13 +0200

    App rename

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper  
   # Return a title on a per-page basis.
     def titled
-      base_title = "Ruby on Rails Tutorial Sample App"
+      base_title = "Feed Me"
       if @title.nil?
         base_title
       else
